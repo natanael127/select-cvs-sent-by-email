9fb8fc97bc138f39c9b20fa78885acca979c65bd
Natanael de Souza Figueiredo <natanael.souza@treetech.com.br>
2020-10-31 15:25:31 -0300

    Numera os candidatos

--- Processa.py
+++ Processa.py
@@ -3,6 +3,7 @@ import datetime
 import json
 import os
 import eml_parser
+import shutil
 
 # ===================== CONSTANTS ============================================ #
 DIR_EMAIL = "E-mails/"
@@ -10,6 +11,7 @@ DIR_CVS   = "Curriculos/"
 DIR_JSON  = "Json/"
 EXT_EMAIL = ".eml"
 EXT_JSON  = ".json"
+FILE_CANDIDATES = "lista.tsv"
 
 # ===================== AUXILIAR FUNCTIONS =================================== #
 def json_serial(obj):
@@ -42,9 +44,12 @@ def break_sender(str_name_email):
     return tuple(str_name_email[:-1].split(" <"))
 
 # ===================== MAIN SCRIPT ========================================== #
+if not os.path.isdir(os.path.dirname(DIR_CVS)):
+        os.makedirs(os.path.dirname(DIR_CVS))
 list_email_files = list_files_by_extension(DIR_EMAIL, EXT_EMAIL)
 for idx in range(len(list_email_files)):
     # Parses to variables
+    idx_str = str(idx + 1).zfill(3)
     email_file = list_email_files[idx]
     email_dict = email_to_dictionary(email_file)
     candidate = {}
@@ -52,14 +57,18 @@ for idx in range(len(list_email_files)):
     candidate["email_address"] = break_sender(email_dict["header"]["header"]["from"][0])[1]
     candidate["cv_filename"] = email_dict["attachment"][0]["filename"]
     # Json file for debug
-    fp = open_creating_dirs(DIR_JSON + str(idx + 1).zfill(3) + EXT_JSON, "w")
+    fp = open_creating_dirs(DIR_JSON + idx_str + EXT_JSON, "w")
     json_str = json.dumps(email_dict, default=json_serial)
     fp.write(json_str)
     fp.close()
-    # Show data
-    if not os.path.exists(DIR_EMAIL + candidate["cv_filename"]):
-        print(str(idx + 1).zfill(3))
+    # Process or warn about some missing file
+    cv_extension = "." + candidate["cv_filename"].split(".")[-1]
+    path_to_received = DIR_EMAIL + candidate["cv_filename"]
+    path_to_organized = DIR_CVS + idx_str + " - " + candidate["name"] + cv_extension
+    if os.path.exists(path_to_received):
+        shutil.copyfile(path_to_received, path_to_organized)
+    else:
+        print(idx_str)
         print(email_file)
         print(candidate)
-        print("FILE ERROR")
         print()
