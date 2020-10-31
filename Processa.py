8ec738154c035bd827c68cb715866f2b1df12fc7
Natanael de Souza Figueiredo <natanael.souza@treetech.com.br>
2020-10-31 09:01:29 -0300

    Prova de conceito para arquivo individual

---
+++ Processa.py
@@ -0,0 +1,22 @@
+import datetime
+import json
+import eml_parser
+
+def json_serial(obj):
+    if isinstance(obj, datetime.datetime):
+        serial = obj.isoformat()
+        return serial
+
+# MAIN
+with open('E-mails/[vagas.fw] CV para vaga estágio - Desenvolvedor em linguagem C.eml', 'rb') as fhdl:
+    raw_email = fhdl.read()
+
+ep = eml_parser.EmlParser()
+parsed_eml = ep.decode_email_bytes(raw_email)
+x = json.dumps(parsed_eml, default=json_serial)
+
+json_file = open('teste.json','w')
+json_file.write(x)
+json_file.close()
+
+print(parsed_eml["header"]["header"]["from"][0])
