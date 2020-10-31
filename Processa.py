10c4790d3706bc0dc813dcc812c5a0d8bf589cfe
Natanael de Souza Figueiredo <natanael.souza@treetech.com.br>
2020-10-31 09:35:35 -0300

    Cria funções para lidar com arquivos e diretórios

--- Processa.py
+++ Processa.py
@@ -1,13 +1,33 @@
 # ===================== IMPORTS ============================================== #
 import datetime
 import json
+import os
 import eml_parser
+
+# ===================== CONSTANTS ============================================ #
+DIR_EMAIL = "E-mails/"
+DIR_CVS   = "Curriculos/"
+EXT_EMAIL = ".eml"
 
 # ===================== AUXILIAR FUNCTIONS =================================== #
 def json_serial(obj):
+    serial = ""
     if isinstance(obj, datetime.datetime):
         serial = obj.isoformat()
-        return serial
+    return serial
+    
+def open_creating_dirs(path, mode):
+    if not os.path.isdir(os.path.dirname(path)):
+        os.makedirs(os.path.dirname(path))
+    fp_itemx = open(path, mode)
+    return fp_itemx
+
+def list_files_by_extension(directory, extension):
+    output_list = []
+    for file in os.listdir(directory):
+        if file.endswith(extension):
+            output_list.append(os.path.join(directory, file))
+    return output_list
 
 # ===================== MAIN SCRIPT ========================================== #
 with open('E-mails/[vagas.fw] CV para vaga estágio - Desenvolvedor em linguagem C.eml', 'rb') as fhdl:
@@ -23,3 +43,5 @@ json_file.close()
 
 print(parsed_eml["header"]["header"]["from"][0])
 print(parsed_eml["attachment"][0]["filename"])
+
+print(list_files_by_extension(DIR_EMAIL, EXT_EMAIL))
