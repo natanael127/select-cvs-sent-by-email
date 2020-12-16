1fde90f240143af3f9a9f47565192d4bdee2b4f0
Natanael de Souza Figueiredo <natanael.souza.figueiredo@gmail.com>
2020-12-15 21:51:45 -0300

    Elimina a necessidade de pasta intermediária para hashes

--- Curriculos.py
+++ Curriculos.py
@@ -9,7 +9,6 @@ import csv
 # ===================== CONSTANTS ============================================ #
 DIR_EMAIL = "E-mails/"
 DIR_CVS   = "Curriculos/"
-DIR_HASH  = "Hashes/"
 EXT_EMAIL = ".eml"
 FILE_CANDIDATES = "lista.csv"
 MSG_ERROR = "Erro ao obter anexo"
@@ -46,18 +45,15 @@ locale.setlocale(locale.LC_ALL, "pt_BR.UTF-8")
 
 # Creates missing directories if does not exist
 create_dir_if_missing(DIR_CVS)
-create_dir_if_missing(DIR_HASH)
 
-# Renames files to theirs sha256 hash
+# Creates a hash table for CVs
 all_input_files = os.listdir(DIR_EMAIL)
-for file_name in all_input_files:
-    extension = get_extension(file_name)
-    input_file_path = DIR_EMAIL + file_name
-    with open(input_file_path, "rb") as f:
-        bytes = f.read() # read entire file as bytes
-    readable_hash = hashlib.sha256(bytes).hexdigest();
-    output_file_path = DIR_HASH + readable_hash + extension
-    shutil.copy(input_file_path, output_file_path)
+files_hash_table = []
+for k in range(len(all_input_files)):
+    files_hash_table.append({})
+    files_hash_table[k]["path"] = DIR_EMAIL + all_input_files[k]
+    bytes = open(files_hash_table[k]["path"], "rb").read()
+    files_hash_table[k]["hash"] = hashlib.sha256(bytes).hexdigest();
 
 # Parses e-mail files
 list_email_files = list_files_by_extension(DIR_EMAIL, EXT_EMAIL)
@@ -97,7 +93,8 @@ for idx_cand in range(len(list_candidates)):
     candidate = list_candidates[idx_cand]
     candidate["Indice"] = str(idx_cand + 1).zfill(3)
     # Process cv or warn about some missing file
-    path_to_received = DIR_HASH + candidate["file_hash"] + candidate["file_extension"]
+    index_hash = [d["hash"] for d in files_hash_table if "hash" in d].index(candidate["file_hash"])
+    path_to_received = files_hash_table[index_hash]["path"]
     path_to_organized = DIR_CVS + candidate["Indice"] + " - " + candidate["Nome"] + candidate["file_extension"]
     if os.path.isfile(path_to_received):
         shutil.copyfile(path_to_received, path_to_organized)
@@ -111,3 +108,4 @@ with open(FILE_CANDIDATES,"w", newline="") as fp:
     dict_writer = csv.DictWriter(fp, csv_keys, delimiter=",", quotechar="\"", quoting=csv.QUOTE_NONNUMERIC, extrasaction="ignore")
     dict_writer.writeheader()
     dict_writer.writerows(list_candidates)
+
